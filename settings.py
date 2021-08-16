e649f23057442d953aa345f87f59890b2aba7457
garbaz <garbaz@t-online.de>
2021-08-16 11:34:28 +0200

    maybe should include the other files...

---
+++ settings.py
@@ -0,0 +1,10 @@
+# The name of the deck where to look for and add cards
+DECK = ">中文<"
+
+# The type of card to add in case no card was found
+CARD_TYPE = "Chinese (Advanced)"
+
+# The name of the field where the word (i.e. the highlighted text, just like with ALT+[1-8]/Skritter)
+# should be inserted. All other fields are left blank.
+# (I use an Anki addon called "Chinese Support Redux" to fill out the rest of the fields)
+WORD_FIELD = "Hanzi"
